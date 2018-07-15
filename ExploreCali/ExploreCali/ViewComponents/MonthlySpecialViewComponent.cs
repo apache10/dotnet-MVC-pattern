8e72a55d95a3cd87814b8ab0bef2b599b4bd5561
apache10 <gagu12ec@gmail.com>
2018-07-15 23:02:11 +1000

    View Component

---
+++ ExploreCali/ExploreCali/ViewComponents/MonthlySpecialViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+namespace ExploreCali.ViewComponents
+{
+    [ViewComponent]
+    public class MonthlySpecialViewComponent :ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
